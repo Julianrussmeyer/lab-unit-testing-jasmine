5a2b1a415489ba02819daae447adb569acdea6d9
Julian Russmeyer <julian@flower.ai>
2026-03-23 21:10:44 +0100

    Solve Lab

--- tests/divide.spec.js
+++ tests/divide.spec.js
@@ -1,14 +1,21 @@
 // UNCOMMENT THE CODE BELOW TO START
 
-/*
-
 describe("Iteration 2", () => {
-    describe("Function - divide", () => {
-        
-        // Each `it` block represents 1 test. You can use the following as a template:
-        it("", () => {});
-
-    })    
-})
-
-*/
+  describe("Function - divide", () => {
+    it("should be defined", () => {
+      expect(divide).toBeDefined();
+    });
+    it("should take two arguments", () => {
+      expect(divide.length).toBe(2);
+    });
+    it("should return the division of the two numbers", () => {
+      expect(divide(4, 2)).toEqual(2);
+      expect(divide(100, 5)).toEqual(20);
+    });
+    it("should return undefined if any of the two arguments is not provided", () => {
+        expect(divide(1)).toEqual(undefined);
+        expect(divide(3, undefined)).toEqual(undefined);
+        expect(divide()).toEqual(undefined);
+      })
+  });
+});
